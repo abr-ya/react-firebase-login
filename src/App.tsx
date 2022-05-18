de24ef9d0612e891d20c457efed9040858dad6a0
abr-ya <gamer05@list.ru>
2022-05-18 21:10:15 +0300

    init: Start project

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+const App = (): JSX.Element => <div className="app">Hello, React!</div>;
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import "regenerator-runtime/runtime";
+import ReactDOM from "react-dom";
+import "./fonts.css";
+import "./body.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
